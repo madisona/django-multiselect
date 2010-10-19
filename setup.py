794f8dba9b49336b00c1f49040bdfe1d1a57358f
Aaron Madison <aaron.madison@theimtgroup.com>
2010-10-19 16:34:15 -0500

    setting up basic buildout config

---
+++ setup.py
@@ -0,0 +1,26 @@
+
+from setuptools import setup
+
+setup(
+    name="django-buildout",
+    version="0.1",
+    description="Django app using buildout",
+    author="Aaron Madison",
+
+    package_dir={'': 'src'},
+    install_requires = (
+        'django==1.2.3',
+        'mock',          # used for testing purposes
+        'pyyaml',        # useful for fixtures and testing
+
+#        'south',         # incredibly useful for database migrations
+#        'coverage',      # useful for continuous integration, tells how much of code is covered by tests 
+#        'clonedigger',   # useful for continuous integration, looks for duplicate chunks of code
+#        'unittest-xml-reporting', # useful for continuous integration, makes nice test results
+#        'pylint',        # useful for continuous integration, looks at code quality
+    ),
+    entry_points=("""
+        [console_scripts]
+        manage=manage:main
+    """)
+)
