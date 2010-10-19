a5fb16978b94dd00b0a090c60b3e29d66aedde07
Aaron Madison <aaron.madison@theimtgroup.com>
2010-10-19 16:44:56 -0500

    changed project name in setup file

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(
-    name="django-buildout",
+    name="django-project",
     version="0.1",
     description="Django app using buildout",
     author="Aaron Madison",
