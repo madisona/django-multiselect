46410099cc557416fef493b39c62969b5e57aadc
Aaron Madison <aaron.madison@theimtgroup.com>
2010-12-01 15:29:21 -0600

    changed generic buildout options

--- setup.py
+++ setup.py
@@ -2,21 +2,13 @@
 from setuptools import setup
 
 setup(
-    name="django-project",
+    name="django-multiselect",
     version="0.1",
-    description="Django app using buildout",
-    author="Aaron Madison",
+    description="Django Buildout application using multiselect",
+    author="Matt Morrison and Aaron Madison",
 
     package_dir={'': 'src'},
     install_requires = (
-        'django==1.2.3',
-        'mock',          # used for testing purposes
-        'pyyaml',        # useful for fixtures and testing
-        'django-debug-toolbar',
-#        'south',         # incredibly useful for database migrations
-#        'coverage',      # useful for continuous integration, tells how much of code is covered by tests 
-#        'clonedigger',   # useful for continuous integration, looks for duplicate chunks of code
-#        'unittest-xml-reporting', # useful for continuous integration, makes nice test results
-#        'pylint',        # useful for continuous integration, looks at code quality
+        'django',
     ),
 )
