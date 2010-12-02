7b569d53f64687b46dccf504b6dd2cf41b4eb6dd
Matthew J Morrison <matt.morrison@theimtgroup.com>
2010-12-02 13:21:10 -0600

    trying something else out in the manifest

--- setup.py
+++ setup.py
@@ -6,11 +6,12 @@ setup(
     version="0.1",
     description="Django Buildout application using multiselect",
     author="Matt Morrison and Aaron Madison",
+    include_package_data=True,
     zip_safe=False, # because we're including media that Django needs
     packages=('multiselect',),
     package_dir={'': 'src'},
     install_requires = (
         'django',
     ),
-    data_files=[('multiselect/media', ['js/ui.multiselect.js', ]), ]
+    #data_files=[('multiselect/media', ['js/ui.multiselect.js', ]), ]
 )
