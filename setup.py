218145ffc6164cc813e3ca2133ca90cb2ba2abf8
Matthew J Morrison <matt.morrison@theimtgroup.com>
2010-12-02 08:15:51 -0600

    removed extra src in path to multiselect package

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     description="Django Buildout application using multiselect",
     author="Matt Morrison and Aaron Madison",
     zip_safe=False, # because we're including media that Django needs
-    packages=('src/multiselect',),
+    packages=('multiselect',),
     package_dir={'': 'src'},
     install_requires = (
         'django',
