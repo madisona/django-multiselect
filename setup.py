1a253145ae6a70e4986c1094f4d7a3f5509ddf8d
Matthew J Morrison <matt.morrison@theimtgroup.com>
2010-12-02 08:13:11 -0600

    moved multiselect back into src directory

--- setup.py
+++ setup.py
@@ -6,10 +6,9 @@ setup(
     version="0.1",
     description="Django Buildout application using multiselect",
     author="Matt Morrison and Aaron Madison",
-    include_package_data=True,
     zip_safe=False, # because we're including media that Django needs
-    packages=('multiselect',),
-    package_dir={'multiselect': 'multiselect', '': '.'},
+    packages=('src/multiselect',),
+    package_dir={'': 'src'},
     install_requires = (
         'django',
     ),
